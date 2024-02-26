22cf210e726e51374eee7959ef9a98c8de084d42
Matteo <matteo.3000@outlook.it>
2024-02-26 16:07:47 +0100

    Aggiungere i file di progetto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EdilCode.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
